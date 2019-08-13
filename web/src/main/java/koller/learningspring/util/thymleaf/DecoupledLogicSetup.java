4fea510f8689f03b43f3eac71c52d8fca6cdd9ce
Jon Koller <jonwkoller@gmail.com>
2019-08-13 12:51:41 -0400

    Removed thymeleaf logic from HTML templates game-over.html, home.html, play.html, and template fragments footer.html and head.html. Created th.xml files for each to included the thymeleaf logic using thymeleaf decoupled template logic after enabling it with new class in util folder DecoupledLogicSetup.

---
+++ web/src/main/java/koller/learningspring/util/thymleaf/DecoupledLogicSetup.java
@@ -0,0 +1,27 @@
+package koller.learningspring.util.thymleaf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+public class DecoupledLogicSetup {
+
+    // -- Fields --
+    private final SpringResourceTemplateResolver templateResolver;
+
+    // -- Constructors --
+    public DecoupledLogicSetup(SpringResourceTemplateResolver templateResolver) {
+        this.templateResolver = templateResolver;
+    }
+
+    // -- Init --
+    @PostConstruct
+    public void init(){
+        templateResolver.setUseDecoupledLogic(true);
+        log.info("Decoupled template logic enabled.");
+    }
+}
